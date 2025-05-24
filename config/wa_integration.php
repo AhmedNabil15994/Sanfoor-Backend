d9dd34074cad2716392e6c687cdf182ac25ab65b
ahmed-nabil94 <ahmednabil15994@gmail.com>
2025-05-24 09:28:15 +0300

    Initial commit

---
+++ config/wa_integration.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'id'            => env('WA_CHANNEL_ID','YOUR_CHANNEL_ID_HERE'),
+    'token'         => env('WA_CHANNEL_TOKEN','YOUR_CHANNEL_TOKEN_HERE'),
+    'identifier'    => env('WA_CHANNEL_IDENTIFIER','YOUR_IDENTIFIER_HERE'),
+];
